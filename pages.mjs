146450a1851d32f928ce7ae98f346eff94831ba3
fres621 <126067139+fres621@users.noreply.github.com>
2025-11-18 21:53:57 +0000

    FIX ghpages oh...

--- pages.mjs
+++ pages.mjs
@@ -6,14 +6,13 @@ function owo(text, pluginId, manifest) {
 
 await cp('pages/common.css', 'dist/common.css');
 const indexTemplate = await readFile('pages/index.html', 'utf-8');
-const pluginTemplate = await readFile('pages/index.html', 'utf-8');
+const pluginTemplate = await readFile('pages/plugin.html', 'utf-8');
 
 const plugs = [];
 for (let plug of await readdir("./plugins")) {
     const manifest = JSON.parse(await readFile(`./plugins/${plug}/manifest.json`));
     plugs.push({ manifest, id: plug });
     await writeFile(`./dist/${plug}/index.html`, owo(pluginTemplate, plug, manifest));
-    await writeFile(`./dist/${plug}.html`, owo(pluginTemplate, plug, manifest));
 }
 
 await writeFile('dist/404.html', indexTemplate.replaceAll(/<(for-each-plugin)>([\s\S]*?)<\/\1>/g, (_, __, template) => {
