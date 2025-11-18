fd0d8e70d0efd072f0db79cb3ec5db145bbfa121
fres621 <126067139+fres621@users.noreply.github.com>
2025-11-18 21:49:08 +0000

    Test new GitHub Pages workflow
    Test new GitHub Pages workflow

---
+++ pages.mjs
@@ -0,0 +1,20 @@
+import { readFile, writeFile, readdir, cp } from "fs/promises";
+
+function owo(text, pluginId, manifest) {
+    return text.replaceAll("{pluginname}", manifest.name).replaceAll("{pluginid}", pluginId);
+}
+
+await cp('pages/common.css', 'dist/common.css');
+const indexTemplate = await readFile('pages/index.html', 'utf-8');
+const pluginTemplate = await readFile('pages/index.html', 'utf-8');
+
+const plugs = [];
+for (let plug of await readdir("./plugins")) {
+    const manifest = JSON.parse(await readFile(`./plugins/${plug}/manifest.json`));
+    plugs.push({ manifest, id: plug });
+    await writeFile(`./dist/${plug}/index.html`, owo(pluginTemplate, plug, manifest));
+}
+
+await writeFile('dist/404.html', indexTemplate.replaceAll(/<(for-each-plugin)>([\s\S]*?)<\/\1>/g, (_, __, template) => {
+    return plugs.map(p => owo(template, p.id, p.manifest)).join('');
+}));
